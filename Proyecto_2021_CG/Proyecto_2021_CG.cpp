d4fe4f243dafe76c9fc9c53e7c47a89497cdf22a
Ivan Marin <imarinr@outlook.com>
2021-01-29 14:21:36 -0600

    En este commit, la letra W, aunque no se dibuja correctamente, ya hace la animacion de caer y rebotar en la pantalla, funciona la tecla espacio. Para volver a ver la animacion, es necesario reiniciar la aplicacion

--- Proyecto_2021_CG/Proyecto_2021_CG.cpp
+++ Proyecto_2021_CG/Proyecto_2021_CG.cpp
@@ -2,18 +2,211 @@
 //
 
 #include <iostream>
+#include <cmath>
+#include <GL/glut.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+/*Constantes de estados posibles*/
+const int EDO_INICIAL = 0;
+const int EDO_CAYENDO = 1;
+const int EDO_GOLPE_SUELO = 2;
+const int EDO_REBOTANDO = 3;
+const int EDO_HALT = 4;
+
+/*Valores iniciales*/
+float x = 10.0, y = 175.0;
+float t = 0.0;
+float xRebote = 0.0;
+float angulo = 0.0;
+int estadoActual = EDO_INICIAL;
+
+/*firmas de funciones*/
+void dibujarW();
+void caidaLibre(float);
+void rebote(float);
+void animacion();
+
+/*
+* funciones necesarias para la visualizacion
+*/
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	
+	//std::cout << "dibujando en ( " << x << ", " << y << " )" << std::endl;
+	glPushMatrix();
+	glTranslatef(x, y, 0);
+	dibujarW();
+	glPopMatrix();
+
+	// prueba de coordenadas
+	glColor3f(0, 0, 0);
+	glPointSize(6);
+	glBegin(GL_POINTS);
+	glVertex2f(0, 0);
+	glVertex2f(199, 199);
+	glVertex2f(0, 199);
+	glVertex2f(199, 0);
+	glVertex2f(100, 100);
+	glEnd();
+
+	glutSwapBuffers();
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
+void init() {
+	glClearColor(0, 0, 0, 0);
+	glColor3f(1, 0, 0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glOrtho(0, 199, 0, 199, -1, 1);
+}
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+/*
+* Funciones del proyecto
+*/
+
+/*
+* Funcion que dibuja la letra W
+* la letra se dibuja a partir del origen de coordenadas definido en la esquina
+* inferior izquierda
+*/
+void dibujarW() {
+	glColor3f(1, 0, 0);
+
+	glBegin(GL_TRIANGLE_FAN);
+	glVertex2f(0, 20);
+	glVertex2f(0, 1);
+	glVertex2f(3, 6);
+	glVertex2f(3, 20);
+	glEnd();
+
+	glColor3f(1, 1, 0.4);
+	glBegin(GL_TRIANGLE_FAN);
+	glVertex2f(5, 0);
+	glVertex2f(11, 20);
+	glVertex2f(6, 20);
+	glVertex2f(0, 0);
+	glEnd();
+
+	glColor3f(1, 0.2, 1);
+	glBegin(GL_TRIANGLE_FAN);
+	glVertex2f(6.25f, 10);
+	glVertex2f(6.25f, 0);
+	glVertex2f(9.25f, 0);
+	glVertex2f(9.25f, 15);
+	glEnd();
+
+	glBegin(GL_TRIANGLE_FAN);
+	glVertex2f(6.25f, 0);
+	glVertex2f(9.25f, 0);
+	glVertex2f(19, 20);
+	glVertex2f(14, 20);
+	glEnd();
+}
+
+/*
+* Funcion de caida libre y(t) = 1/2 g t^2
+* se calcula el valor respecto al tiempo y se resta de la posicion vertical actual
+* Cuando y llega a cero, cambia el estado para detener la animacion
+*/
+void caidaLibre(float t) {
+	//calcular caida libre
+	//std::cout << "caidaLibre( t = " << t << ", y = " << y << " )" << std::endl;
+	float yn = 0.5 * 9.81 * pow(t, 2);
+	//restar de la posicion actual
+	y -= yn;
+	if (y <= 0) {
+		estadoActual = EDO_GOLPE_SUELO;
+		std::cout << "golpe en el suelo" << std::endl;
+		y = 0;
+	}
+}
+
+/*
+* Funcion de rebote.
+* Calcula la posicion y de la W durante un espacio de valores x
+*/
+void rebote(float t) {
+	if (x <= 199) {
+		y = 180 * exp(-0.1 * t) * fabs(sin((2*3.14159/20) * t));
+	}
+	else {
+		y = 0;
+		estadoActual = EDO_HALT;
+	}
+}
+
+/*
+/ Funcion que ejecuta los calculos de las animaciones dependiendo del
+* estado actual
+*/
+void animacion() {
+	switch (estadoActual)
+	{
+	case EDO_INICIAL:
+		break;
+	case EDO_CAYENDO:
+		//std::cout << "t = " << t << ", y = " << y << std::endl;
+		caidaLibre(t);
+		t += 0.01;
+		glutPostRedisplay();
+		break;
+	case EDO_GOLPE_SUELO: // por el momento pasamos directo al rebote
+		estadoActual = EDO_REBOTANDO;
+		t = 0;
+		break;
+	case EDO_REBOTANDO:
+		rebote(t);
+		x += 0.5;
+		t += 0.1;
+		std::cout << x << "," << y << std::endl;
+		glutPostRedisplay();
+		break;
+	case EDO_HALT:
+		break;
+	default:
+		break;
+	}
+}
+/*funcion de teclas especiales*/
+void teclado(unsigned char key, int x, int y) {
+	switch (key)
+	{
+	case ' ': // se presiona la tecla espacio
+		if (estadoActual == EDO_INICIAL)
+		{
+			estadoActual = EDO_CAYENDO;
+			std::cout << "tecla espacio presionada, cayendo" << std::endl;
+		}
+		break;
+	case 27: // salir presinando escape
+		exit(0);
+		break;
+	default:
+		break;
+	}
+	glutPostRedisplay();
+}
+
+int main(int argc, char** argv)
+{
+    std::cout << "Universidad Nacional Autónoma de México" << std::endl;
+	std::cout << "Facultad de Ingeniería" << std::endl;
+	std::cout << "Computación Gráfica. Semestre 2021-1" << std::endl;
+	std::cout << "Marin Roldan Ivan" << std::endl;
+	std::cout << "-------------------------------------------------" << std::endl;
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+	glutInitWindowSize(800, 600);
+	glutInitWindowPosition(150, 50);
+	glutCreateWindow("Computacion Grafica. Semestre 2021-1. Marin Roldan Ivan");
+
+	init();
+
+	glutDisplayFunc(display);
+	glutKeyboardFunc(teclado);
+	glutIdleFunc(animacion);
+
+	
+	glutMainLoop();
+
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+# Proyecto de Computacion Grafica. Semestre 2021-1
+**Facultad de Ingenieria UNAM**
+**Ivan Marin Roldan**
+
+# Vision General (Overview)
+
+Este Proyecto se realizo como parte de la materia de Computacion Grafica para el semestre 2020-1.
+
+Se trata de una animación de una letra **'W'** que cae y hace el efecto de rebotar.
+
+# Como ejecutar el proyecto (installation)
+
+## Pre-requisitos
+
+Este proyecto fue creado con OpenGL 3.7 beta y Visual Studio Comunity 2017. Para configurar su entorno de desarrollo, por favor, siga las instrucciones disponibles en el siguiente [tutorial](https://www.absingh.com/opengl/)
+
+## Agregar archivo de codigo a la solucion
+
+En el tutorial anterior, queda configurado un proyecto, en el que se puede agregar el archivo _'Proyecto_2021_CG.cpp'_ que contiene la funcion main y la logica del programa completo.
+
+A partir de este punto, sientase libre de compilar y ejecutar el proyecto para poder visualizar la animacion.
+
+# Uso del programa (usage)
+
+Una vez que el programa se pone en ejecucion, se mostrara la letra **'W'** en una posicion fija. Para reproducir la animacion, presione la tecla *'espacio'* y la animacion empezara su curso.
+
+# Licencia (License)
+
